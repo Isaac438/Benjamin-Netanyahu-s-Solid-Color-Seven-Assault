1e5521398cfca7ed0df582a4cbbab764bc2090b7
Isaac <isaacflenner@icloud.com>
2026-04-22 21:34:40 -0400

    Added switching between maps

--- prototypes/preload-2026-04-19-21-27-23/global.gd
+++ prototypes/preload-2026-04-19-21-27-23/global.gd
@@ -3,4 +3,4 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var soft_wall_health = 100
+	pass
---
+++ prototypes/preload-2026-04-19-21-27-23/scenes/map_root.gd
@@ -0,0 +1,25 @@
+extends Node3D
+
+var maps = [
+	preload("res://scenes/oregani.tscn"),
+	preload("res://scenes/island.tscn"),
+	preload("res://scenes/debug_map.tscn")
+]
+
+var current_map: Node = null
+var index := 0
+
+func _ready():
+	switch_to_map(1) # start on island if you want
+
+func _input(event):
+	if event is InputEventKey and event.pressed and event.keycode == KEY_P:
+		index = (index + 1) % maps.size()
+		switch_to_map(index)
+
+func switch_to_map(i: int):
+	if current_map:
+		current_map.queue_free()
+
+	current_map = maps[i].instantiate()
+	add_child(current_map)
