f43c5f14df098b5372cee3a8a7e2fe567bf6a3f5
Isaac438 <80593287+Isaac438@users.noreply.github.com>
2026-05-07 09:38:13 -0400

    Refactor tracer logic for raycasting and collision

--- prototypes/Multiplayer Test/scenes/tracer.gd
+++ prototypes/Multiplayer Test/scenes/tracer.gd
@@ -1,15 +1,22 @@
-extends CharacterBody3D
+var from = global_position
+var to = from + direction * speed * delta
 
-var speed = 50.0
-var direction = Vector3.ZERO
+var space = get_world_3d().direct_space_state
 
-func _physics_process(delta):
-	var motion = direction * speed * delta
-	var collision = move_and_collide(motion)
-	if collision:
-		var body = collision.get_collider()
-		if body.get_parent().has_method("die"):
-			body.get_parent().die()
+var query = PhysicsRayQueryParameters3D.create(from, to)
+query.exclude = [self]  # optional: ignore the bullet itself
+
+var result = space.intersect_ray(query)
+
+if result:
+	var body = result.collider
+
+	if body.has_method("die"):
+		body.die()
 		queue_free()
-	await get_tree().create_timer(3).timeout
-	queue_free()
+	elif body.has_method("puncture"):
+		body.puncture(global_position)
+	else:
+    	queue_free()
+else:
+    global_position = to
