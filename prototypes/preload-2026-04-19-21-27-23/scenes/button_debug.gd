f311e403ffca5e92854a5fbfe65aa343773e0f98
Isaac438 <isaacflenner@icloud.com>
2026-08-10 14:10:57 -0400

    removed unnecessary files

--- prototypes/preload-2026-04-19-21-27-23/scenes/button_debug.gd
+++
@@ -1,9 +0,0 @@
-extends Button
-
-@export var map_index := 2
-
-func _ready():
-	pressed.connect(_on_pressed)
-
-func _on_pressed():
-	global.set_map(map_index)
--- prototypes/preload-2026-04-19-21-27-23/scripts/spawn.gd
+++
@@ -1,5 +0,0 @@
-extends Node3D
-
-func get_spawn():
-	var spawns = $Spawns.get_children()
-	return spawns[randi_range(0, spawns.size() - 1)]
