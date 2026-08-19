931a4049855804ee6e9003d376baa5950d573089
isaacnewtonsard <isaacflenner@icloud.com>
2026-08-18 23:07:04 -0400

    fixed the slider

--- prototypes/Multiplayer Test/scripts/fsr_magnitude_slider.gd
+++ prototypes/Multiplayer Test/scripts/fsr_magnitude_slider.gd
@@ -3,5 +3,15 @@ extends HSlider
 func _ready() -> void:
 	get_viewport().scaling_3d_scale = 1.0
 
+var ticks: float = 0.0
+func _process(delta: float) -> void:
+	ticks+=delta
+	if ticks >= 1.0:
+		value_changed(value)
+
 func value_changed(value: float):
-	get_viewport().scaling_3d_scale = value
+	if value == 1.0:
+		get_viewport().scaling_3d_mode = Viewport.SCALING_3D_MODE_BILINEAR
+		get_viewport().scaling_3d_scale = 1.0
+	else:
+		get_viewport().scaling_3d_scale = value
